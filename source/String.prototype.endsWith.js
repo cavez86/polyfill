8fe6e83111c063b4ef9696404437f225cbed6f90
cavez86 <cavez86@users.noreply.github.com>
2016-10-26 12:31:11 +0200

    Fixed wrong logic

--- source/String.prototype.endsWith.js
+++ source/String.prototype.endsWith.js
@@ -2,5 +2,5 @@
 String.prototype.endsWith = function (string) {
 	var index = arguments.length < 2 ? this.length : arguments[1];
 
-	return this.indexOf(string) === index - string.length;
+	return this.lastIndexOf(string) === index - string.length;
 };
